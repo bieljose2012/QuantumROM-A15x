2e4a8f4c8fd340a05def0407ac161cffaf6e850b
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-09-18 08:32:50 +0600

    Update make_rom.sh

--- make_rom.sh
+++ make_rom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install required packages.
-sudo bash scripts/install_packages.sh
+# sudo bash scripts/install_packages.sh
 
 # Download stock firmware.
 sudo bash scripts/download_firmware.sh SM-A225F BKD 350167020473859 Stock
