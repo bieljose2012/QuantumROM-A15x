585ab11b336dc24f9616379f2869a36409f2b74f
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-09-18 13:05:20 +0600

    Update download_a225x_firmware.sh

--- QuantumROM/Devices/SM-A225X/download_a225x_firmware.sh
+++ QuantumROM/Devices/SM-A225X/download_a225x_firmware.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+if [ "$#" -ne 4 ]; then
+    echo "Usage: bash $0 MODEL CSC IMEI DOWNLOAD_FOLDER"
+    exit 1
+fi
+
+MODEL=$1
+CSC=$2
+IMEI=$3
+
 # Download stock firmware.
-sudo bash scripts/download_firmware.sh SM-A225F BKD 350167020473859 Stock
+sudo bash scripts/download_firmware.sh $MODEL $CSC $IMEI $MODEL
