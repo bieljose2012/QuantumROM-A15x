af6935b6932f8fffbde1138fd054357e3922a7d0
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-09-22 08:58:20 +0600

    Update pack_ext4.sh

--- scripts/pack_ext4.sh
+++ scripts/pack_ext4.sh
@@ -22,27 +22,27 @@ for folder_path in "$rom_directory"/*; do
         echo "Skipping config folder..."
         continue
     fi
- 
+
     partition_name="$folder_name"
     file_contexts_file="$rom_directory/config/$folder_name/${folder_name}_file_contexts"
     fs_config_file="$rom_directory/config/$folder_name/${folder_name}_fs_config"
     SIZE=$(du -sb "$rom_directory/$folder_name" | awk '{printf "%d", $1 * 1.07}')
-	
-	if [ "$folder_name" = "system" ]; then
+
+    if [ "$folder_name" = "system" ]; then
         mount_point="/"
     else
         mount_point="/$folder_name"
     fi
-	
+
     echo ""
     echo "Creating $partition_name.img from $folder_path..."
-sort -u "$file_contexts_file" -o "$file_contexts_file"
-sort -u "$fs_config_file" -o "$fs_config_file"
+    sort -u "$file_contexts_file" -o "$file_contexts_file"
+    sort -u "$fs_config_file" -o "$fs_config_file"
     ./bin/make_ext4fs -J -T -1 \
         -S "$file_contexts_file" \
         -C "$fs_config_file" \
         -l "$SIZE" \
-        -L "$mountpoint" \
+        -L "$mount_point" \
         -a "$partition_name" \
         "$out_dir/$partition_name.img" "$rom_directory/$partition_name"
 done
