effd41f0d66f4f32acf8d7221a4f7c1d19bab79f
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-09-22 13:04:52 +0600

    Update SM-A225F.sh

--- QuantumROM/Devices/SM-A225F/SM-A225F.sh
+++ QuantumROM/Devices/SM-A225F/SM-A225F.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+if [ "$#" -ne 4 ]; then
+    echo "Usage: bash $0 MODEL CSC IMEI DOWNLOAD_FOLDER"
+    exit 1
+fi
+
+MODEL=SM-A225F
+CSC=BKD
+IMEI=350167020473859
+
+# Download stock firmware.
+sudo bash scripts/download_firmware.sh $MODEL $CSC $IMEI $MODEL
