0045dd664ef657b681c991c01271ca5d6cd7ab7c
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-09-23 09:21:29 +0600

    Update security_disabler.sh

--- QuantumROM/mods/security_disabler.sh
+++ QuantumROM/mods/security_disabler.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+set -e
 
+if [ "$#" -ne 1 ]; then
+    echo "Usage: bash $0 Device_Model"
+    exit 1
+fi
+
+MODEL=$1
+
+echo "Deleting stock recovery."
+rm -f "../fw_download/$MODEL/vendor/recovery-from-boot.p"
