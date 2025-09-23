a8f43560875cde08884e9ccfcbde03ea99ad1579
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-09-23 09:50:47 +0600

    Add files via upload

---
+++ QuantumROM/mods/musti_disabler.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+set -e
+
+#
+# A simple Samsung services disabler by Ian Macdonald.
+#
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: bash $0 Device_Model"
+    exit 1
+fi
+
+MODEL=$1
+
+disable_fbe() {
+  local md5
+  local i
+  fstab_files=`grep -lr 'fileencryption' ../../fw_download/$MODEL/vendor/etc`
+
+  #
+  # Exynos devices = fstab.exynos*.
+  # MediaTek devices = fstab.mt*.
+  # Snapdragon devices = fstab.qcom, fstab.emmc, fstab.default
+  #
+  for i in $fstab_files; do
+    if [ -f $i ]; then
+      echo " - Disabling file-based encryption (FBE) for /data..."
+      echo " -   Found $i."
+      # This comments out the offending line and adds an edited one.
+      sed -i -e 's/^\([^#].*\)fileencryption=[^,]*\(.*\)$/# &\n\1encryptable\2/g' $i
+    fi
+  done
+}
+
+disable_fde() {
+  local md5
+  local i
+  fstab_files=`grep -lr 'forceencrypt' ../../fw_download/$MODEL/vendor/etc`
+
+  #
+  # Exynos devices = fstab.exynos*.
+  # MediaTek devices = fstab.mt*.
+  # Snapdragon devices = fstab.qcom, fstab.emmc, fstab.default
+  #
+  for i in $fstab_files; do
+    if [ -f $i ]; then
+      echo " - Disabling full-disk encryption (FDE) for /data..."
+      echo " -   Found $i."
+      md5=$( md5 $i )
+      # This comments out the offending line and adds an edited one.
+      sed -i -e 's/^\([^#].*\)forceencrypt=[^,]*\(.*\)$/# &\n\1encryptable\2/g' $i
+      file_changed $i $md5
+    fi
+  done
+}
+
+
+echo " "
+echo "Multi-disabler v3.1 for Samsung devices"
+echo "running Android 9 or later."
+echo "by Ian Macdonald, enhanced by afaneh92"
+echo " "
+
+disable_fbe
+disable_fde
--- QuantumROM/mods/security_disabler.sh
+++ QuantumROM/mods/security_disabler.sh
@@ -8,5 +8,14 @@ fi
 
 MODEL=$1
 
-echo "Deleting $MODEL stock recovery."
+
+# Product
+# Deleting frp line from $MODEL product build.prop.
+sed -i "\~ro.frp.pst=/dev/block/bootdevice/by-name/frp~d" ../../fw_download/$MODEL/product/etc/build.prop
+
+# Vendor
+
+# Deleting $MODEL stock recovery.
 rm -f "../../fw_download/$MODEL/vendor/recovery-from-boot.p"
+# Runnig multidisabeler.
+bash ./musti_disabler.sh $MODEL
