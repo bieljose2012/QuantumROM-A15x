bd90680732b86010c55f5c579496b3c3206ce4c0
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-09-25 22:25:11 +0600

    Update security_disabler.sh

--- QuantumROM/mods/security_disabler.sh
+++ QuantumROM/mods/security_disabler.sh
@@ -8,13 +8,11 @@ fi
 
 MODEL=$1
 
-
 # Product
 # Deleting frp line from $MODEL product build.prop.
 sed -i "\~ro.frp.pst=/dev/block/bootdevice/by-name/frp~d" ../../fw_download/$MODEL/product/etc/build.prop
 
 # Vendor
-
 # Deleting $MODEL stock recovery.
 rm -f "../../fw_download/$MODEL/vendor/recovery-from-boot.p"
 # Runnig multidisabeler.
