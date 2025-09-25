e813acc1a5d7b6b9deb0371f5fd9d3aeed8a38cc
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-09-25 22:45:11 +0600

    Update make_rom.sh

--- make_rom.sh
+++ make_rom.sh
@@ -1,3 +1,7 @@
 # Run Debloat cmd.
 chmod +x ./QuantumROM/mods/debloater.sh
-sudo bash ./QuantumROM/mods/debloater.sh
+bash ./QuantumROM/mods/debloater.sh
+
+# Run security disabler cmd.
+chmod +x ./QuantumROM/mods/security_disabler.sh
+bash ./QuantumROM/mods/security_disabler.sh
