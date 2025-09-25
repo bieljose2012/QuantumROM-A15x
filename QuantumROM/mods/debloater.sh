8302fb37990cadfa8a11af40d1382aa8d262766c
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-09-25 22:36:37 +0600

    Update debloater.sh

--- QuantumROM/mods/debloater.sh
+++ QuantumROM/mods/debloater.sh
@@ -1,1 +1,9 @@
-
+# Debloat $MODEL Product.
+rm -rf "../../fw_download/$MODEL/product/app/DuoStub"
+rm -rf "../../fw_download/$MODEL/product/app/GoogleCalendarSyncAdapter"
+rm -rf "../../fw_download/$MODEL/product/app/SpeechServicesByGoogle"
+rm -rf "../../fw_download/$MODEL/product/priv-app/AndroidAutoStub"
+rm -rf "../../fw_download/$MODEL/product/priv-app/AndroidSystemIntelligence"
+rm -rf "../../fw_download/$MODEL/product/priv-app/GoogleRestore"
+rm -rf "../../fw_download/$MODEL/product/priv-app/Messages"
+rm -rf "../../fw_download/$MODEL/product/priv-app/SearchSelector"
