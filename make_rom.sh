7b6f2515d22d9b65e20c3ce8b02f183ea23b4ef6
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-09-26 17:23:26 +0600

    Update make_rom.sh

--- make_rom.sh
+++ make_rom.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: bash $0 Device_Model"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: bash $0 MODEL CSC IMEI"
     exit 1
 fi
 
 MODEL=$1
+CSC=$2
+IMEI=$3
 
 # Run Debloat cmd.
 chmod +x ./QuantumROM/mods/debloater.sh
