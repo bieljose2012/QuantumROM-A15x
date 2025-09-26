d65467230af940d919f74b741a985f4ffe3fedae
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-09-26 08:52:33 +0600

    Update debloater.sh

--- QuantumROM/mods/debloater.sh
+++ QuantumROM/mods/debloater.sh
@@ -1,3 +1,10 @@
+if [ "$#" -ne 1 ]; then
+    echo "Usage: bash $0 Device_Model"
+    exit 1
+fi
+
+MODEL=$1
+
 # Debloat $MODEL Product.
 rm -rf "../../fw_download/$MODEL/product/app/DuoStub"
 rm -rf "../../fw_download/$MODEL/product/app/GoogleCalendarSyncAdapter"
