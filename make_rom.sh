ffbc29cd0bee4d3935700b21587892c67a339085
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-09-26 14:27:36 +0600

    Update make_rom.sh

--- make_rom.sh
+++ make_rom.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+set -e
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: bash $0 Device_Model"
+    exit 1
+fi
+
+MODEL=$1
+
 # Run Debloat cmd.
 chmod +x ./QuantumROM/mods/debloater.sh
 bash ./QuantumROM/mods/debloater.sh
