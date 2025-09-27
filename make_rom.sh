6e067d8b61c24def7b1cfd86b99da1b60ed319cb
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-09-27 06:34:11 +0600

    Add files via upload

--- make_rom.sh
+++ make_rom.sh
@@ -11,33 +11,42 @@ CSC=$2
 IMEI=$3
 FW_DIR="fw_download"
 
+
 # --- Setup Directories ---
 chmod +x ./scripts/download_firmware.sh
 bash ./scripts/setup_directories.sh
 
+
 # --- Start firmware download ---
 bash ./scripts/download_firmware.sh "$MODEL" "$CSC" "$IMEI" "$MODEL"
 
+
 # --- Extract Firmware ---
 chmod +x ./scripts/extract_firmware.sh
-bash ./scripts/extract_firmware.sh "$(pwd)/${FW_DIR}/${MODEL}" "${MODEL}_${CSC}.zip"
+bash ./scripts/extract_firmware.sh "$(pwd)/${FW_DIR}/${MODEL}" "${MODEL}.zip"
 
 
 # --- Run IMG Unpack cmd ---
 chmod +x ./scripts/extract_ext4.sh
-bash ./scripts/extract_ext4.sh "$(pwd)/${FW_DIR}/${MODEL}" "${MODEL}_${CSC}"
+bash ./scripts/extract_ext4.sh "$(pwd)/${FW_DIR}/${MODEL}" "${MODEL}"
+
 
 # --- Run Debloat cmd ---
 chmod +x ./QuantumROM/mods/debloater.sh
-bash ./QuantumROM/mods/debloater.sh "$(pwd)/${FW_DIR}/${MODEL}/${MODEL}_${CSC}"
+bash ./QuantumROM/mods/debloater.sh "$(pwd)/${FW_DIR}/${MODEL}/${MODEL}"
+
 
 # --- Run Security Disabler cmd ---
 chmod +x ./QuantumROM/mods/security_disabler.sh
-bash ./QuantumROM/mods/security_disabler.sh "$(pwd)/${FW_DIR}/${MODEL}/${MODEL}_${CSC}"
+chmod +x ./QuantumROM/mods/musti_disabler.sh
+bash ./QuantumROM/mods/security_disabler.sh "$(pwd)/${FW_DIR}/${MODEL}/${MODEL}"
+bash ./QuantumROM/mods/musti_disabler.sh "$(pwd)/${FW_DIR}/${MODEL}/${MODEL}"
+
 
 # --- Run IMG Pack cmd ---
 chmod +x ./scripts/pack_ext4.sh
-bash ./scripts/pack_ext4.sh "$(pwd)/${FW_DIR}/${MODEL}" "${MODEL}_${CSC}"
+bash ./scripts/pack_ext4.sh "$(pwd)/${FW_DIR}/${MODEL}" "${MODEL}"
+
 
 # --- Upload images in Google Drive ---
 # python3 upload_files.py
