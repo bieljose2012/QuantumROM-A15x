83431828bb5fb456efed77671d91a6c63228299a
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-09-28 16:50:46 +0600

    Update install_packages.sh

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-# if [[ "$OSTYPE" == "linux-android"* ]] || [[ "$(uname -o)" == "Android" ]]; then
-#    echo "Detected Termux (Android)"
-#    # Termux setup:
-#    pkg update -y && pkg upgrade -y
-#    pkg install -y tsu git mount-utils android-tools blk-utils p7zip lz4 wget python3
-
-# elif [[ "$(uname -s)" == "Linux" ]]; then
-#    echo "Detected Linux"
-#    # Linux setup:
-#    apt update -y && apt upgrade -y
-#    apt install -y p7zip-full lz4 android-sdk-libsparse-utils wget python3 python3-pip
-# else
-#    echo "Unsupported OS"
-#    exit 1
-# fi
-
 # Linux setup:
 apt update -y && apt upgrade -y
 apt install -y p7zip-full lz4 android-sdk-libsparse-utils wget python3 python3-pip
